97953c986c4136a0cc411ebb7da61bd18b2e80a0
davidAllenStephan <david.marino03@hotmail.com>
2024-02-06 16:59:43 -0600

    added the student class

--- src/edu/mu/student/Student.java
+++ src/edu/mu/student/Student.java
@@ -2,4 +2,47 @@ package edu.mu.student;
 
 public class Student {
 	private int id;
+	private String name;
+	private double grade;
+
+	public Student() {}
+
+	public Student(int id, String name, double grade) {
+		this.id = id;
+		this.name = name;
+		this.grade = grade;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+	
+	@Override
+	public String toString() {
+		return "id: " + this.id + ", name: " + this.name + ", grade: " + this.grade;
+	}
+	
+	
+
+
 }
