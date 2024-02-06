3f4e30673c5d49428153fc7a7e7e5c6166c70743
Sydney Poulsen <sydpoulsen@yahoo.com>
2024-02-06 17:07:39 -0600

    added student array to StudentManager

--- src/edu/mu/student/StudentManager.java
+++ src/edu/mu/student/StudentManager.java
@@ -1,5 +1,13 @@
 package edu.mu.student;
 
 public class StudentManager {
+	private StudentManager[] students;
+	
+	public StudentManager() {
+		this.students = new StudentManager[];
+	}
+}
 
+public boolean readFromFile(String fileName) {
+	
 }
